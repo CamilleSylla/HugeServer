a2171a6d305531986eb770d690533f493b7e4edd
CamilleSylla <camillesylla.web@gmail.com>
2020-07-27 10:14:58 +0200

    configured knex with server got an issue with parser

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const PORT = process.env.PORT || 3001;
 const knex = require('./knex/knex');
+const knexfile = require('./knexfile').development;
 
 
+const db = knex(knexfile);
+
+db.select('*').from('users').then(date => {
+  console.log(data);
+})
 const app = express(); 
 
+app.use(cors());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extends: false}));
 
+app.post('/signup', (req, res) => {
+  const { name, email } = req.body;
+  db('users').insert({
+    name: name,
+    email: email,
+  }).then(console.log)
+})
 
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`);
