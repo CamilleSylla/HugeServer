c65bb0057e524e80022df3db54d0ffb190cfe736
CamilleSylla <camillesylla.web@gmail.com>
2020-08-01 16:45:47 +0200

    started to create load user and creat the req to dasboardid

--- server.js
+++ server.js
@@ -32,7 +32,12 @@ app.get('/dashboard/:id', (req, res) => {
   const { id } = req.params;
   db.select('*').from('users').where({ id })
   .then(user => {
+    if(user.lenght){
       res.json(user[0])
+    }else{
+      res.status(400).json('Not found')
+    }
+      
   })
   .catch(err => res.status(400).json('error getting data'))
 });
